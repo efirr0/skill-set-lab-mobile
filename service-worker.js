3caef55a46832be65f052e5d81d6564a7c7b9fde
efirr0 <fushi1215@gmail.com>
2026-05-05 20:57:09 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "skill-set-lab-v1";
+const ASSETS = ["./skill_set_mobile.html", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
