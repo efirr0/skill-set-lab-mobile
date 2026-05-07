f5c7800ad8a737806df386838c9901a4e836d163
efirr0 <fushi1215@gmail.com>
2026-05-08 00:05:26 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "skill-set-lab-20260506-3";
+const CACHE_NAME = "skill-set-lab-20260507-1";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
